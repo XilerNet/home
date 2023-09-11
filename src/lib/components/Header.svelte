59dee2a84404cefde96d3f369dda19a6b1087bf8
Arthurdw <dev@arthurdw.com>
2023-09-11 18:34:21 +0200

    :construction: Working on the basket

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -16,33 +16,61 @@
 
     $: isSignedIn = api.isSignedIn;
 
+    // TODO: Dynamic basket detection
+    let hasItemInBasket = false;
+
     api.initSignedIn().then();
 </script>
 
 
 <header>
-    <a class="xiler" href="/" use:link>
-        <img src="/logo.svg" alt="logo">
-        <p>Xiler <span>alpha</span></p>
-    </a>
+    <div class="default-menu">
+        <a class="xiler" href="/" use:link>
+            <img src="/logo.svg" alt="logo">
+            <p>Xiler <span>alpha</span></p>
+        </a>
 
-    <nav>
-        <ul>
-            <li><a href="/" use:link>Home</a></li>
-            <li><a href="/docs" use:link>Documentation</a></li>
-            <li><a href="https://dc.xiler.net" target="_blank">Support</a></li>
-        </ul>
-    </nav>
+        <nav>
+            <ul>
+                <li><a href="/" use:link>Home</a></li>
+                <li><a href="/docs" use:link>Documentation</a></li>
+                <li><a href="https://dc.xiler.net" target="_blank">Support</a></li>
+            </ul>
+        </nav>
 
-    <div class="login">
-        {#if $isSignedIn}
-            <button class="logout" on:click={logout}>Logout</button>
-        {:else}
-            <button on:click={login}>Connect Wallet</button>
-        {/if}
+        <div class="login">
+            {#if $isSignedIn}
+                <button class="logout" on:click={logout}>Logout</button>
+            {:else}
+                <button on:click={login}>Connect Wallet</button>
+            {/if}
+        </div>
     </div>
+
+    {#if $isSignedIn}
+        <div class="menu-bar">
+            <nav>
+                <ul>
+                    <li><a href="/me/domains" use:link>Domains</a></li>
+                    <li><a href="/me/wallets" use:link>Wallets</a></li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="/me/basket" use:link class="basket {hasItemInBasket ? 'full' : ''}">
+                            <img src="/media/cart.svg" alt="basket">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/me" use:link>
+                            <img src="/media/user.svg" alt="user">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    {/if}
 </header>
-<div class="spacer"></div>
+<div class="{$isSignedIn ? 'spacer-large' : 'spacer-small'}"></div>
 
 <style lang="scss">
   $header-side-spacing: 4dvw;
@@ -56,121 +84,157 @@
     top: 0;
     left: 0;
 
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    width: calc(100dvw - #{$header-side-spacing} * 2);
-    height: fit-content;
-    padding: 1.25rem #{$header-side-spacing};
-
-    background-color: $background-color;
-
-    .xiler {
+    .menu-bar,
+    .default-menu {
       display: flex;
-
       align-items: center;
-      gap: 0.875rem;
+      justify-content: space-between;
 
-      width: fit-content;
+      width: calc(100dvw - #{$header-side-spacing} * 2);
       height: fit-content;
+      padding: 1.25rem #{$header-side-spacing};
 
-      text-decoration: none;
+      background-color: $background-color;
 
-      img, p {
-        display: block;
-      }
+      .xiler {
+        display: flex;
 
-      img {
-        width: 2.625rem;
-        height: 2.625rem;
-      }
+        align-items: center;
+        gap: 0.875rem;
 
-      p {
-        position: relative;
+        width: fit-content;
+        height: fit-content;
 
-        padding: 0;
-        margin: 0;
+        text-decoration: none;
 
-        color: $text-color;
+        img, p {
+          display: block;
+        }
 
-        font-size: 1.563rem;
-        font-weight: 700;
-        font-family: 'Quicksand', sans-serif;
+        img {
+          width: 2.625rem;
+          height: 2.625rem;
+        }
 
-        span {
-          position: absolute;
-          font-size: 0.7rem;
+        p {
+          position: relative;
 
-          bottom: -50%;
-          right: -25%;
+          padding: 0;
+          margin: 0;
+
+          color: $text-color;
+
+          font-size: 1.563rem;
+          font-weight: 700;
+          font-family: 'Quicksand', sans-serif;
+
+          span {
+            position: absolute;
+            font-size: 0.7rem;
+
+            bottom: -50%;
+            right: -25%;
+          }
         }
       }
-    }
 
-    nav {
-      display: flex;
-      justify-content: center;
-
-
-      ul {
+      nav {
         display: flex;
-        // TODO: Dynamic
-        gap: 1.25rem;
+        justify-content: center;
 
-        li {
-          list-style: none;
 
-          a {
-            text-decoration: none;
-            color: $text-color;
-            font-size: 0.938rem;
-            font-weight: 600;
-            font-family: 'Quicksand', sans-serif;
+        ul {
+          display: flex;
+          // TODO: Dynamic
+          gap: 1.25rem;
 
-            transition: color 0.2s ease-in-out;
+          li {
+            list-style: none;
 
-            &:hover {
-              color: $accent-color;
+            a {
+              text-decoration: none;
+              color: $text-color;
+              font-size: 0.938rem;
+              font-weight: 600;
+              font-family: 'Quicksand', sans-serif;
+
+              transition: color 0.2s ease-in-out;
+
+              &:hover {
+                color: $accent-color;
+              }
             }
+          }
+        }
+      }
+
+      .login button {
+        font-family: 'Inter', sans-serif;
+        font-size: 0.938rem;
+        font-weight: 600;
+
+        color: $text-color;
+        background-color: $accent-color;
+        padding: 0.75rem 1.125rem;
+
+        border: none;
+        border-radius: 0.375rem;
+
+        cursor: pointer;
+
+        transition: background-color 0.2s ease-in-out;
+
+        &:hover {
+          background-color: $dark-accent-color;
+        }
+
+        &.logout {
+          color: $accent-color;
+          background-color: $background-color;
+          transition: color 0.2s ease-in-out;
+
+          &:hover {
+            color: $dark-accent-color;
           }
         }
       }
     }
 
-    .login button {
-      font-family: 'Inter', sans-serif;
-      font-size: 0.938rem;
-      font-weight: 600;
+    .menu-bar {
+      //padding-top: 0;
+      padding-top: 0.15rem;
 
-      color: $text-color;
-      background-color: $accent-color;
-      padding: 0.75rem 1.125rem;
+      nav {
+        width: 100%;
+        justify-content: space-between;
+        padding-top: 1.25rem;
 
-      border: none;
-      border-radius: 0.375rem;
+        border-top: 0.063rem solid #2F3030;
 
-      cursor: pointer;
 
-      transition: background-color 0.2s ease-in-out;
+        .basket {
+          position: relative;
 
-      &:hover {
-        background-color: $dark-accent-color;
-      }
-
-      &.logout {
-        color: $accent-color;
-        background-color: $background-color;
-        transition: color 0.2s ease-in-out;
-
-        &:hover {
-          color: $dark-accent-color;
+          &.full::after {
+            content: "";
+            position: absolute;
+            top: -0.575rem;
+            right: -0.05rem;
+            width: 0.6rem;
+            height: 0.6rem;
+            background-color: #E84C3D;
+            border-radius: 50%;
+          }
         }
       }
     }
   }
 
-  .spacer {
+  .spacer-small {
     height: 5.125rem;
+  }
+
+  .spacer-large {
+    height: 9.625rem;
   }
 </style>
