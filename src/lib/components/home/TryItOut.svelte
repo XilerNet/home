e7e6ab35fec6a7024436ca451731bdacea047cc7
Arthurdw <dev@arthurdw.com>
2023-09-11 20:41:28 +0200

    :construction: Prep for tomorrow try it out section

---
+++ src/lib/components/home/TryItOut.svelte
@@ -0,0 +1,21 @@
+<article>
+    <h4> Don’t take our word for it, try it out! </h4>
+    <p>While many projects make promises they can't keep, we've taken a different approach. We've already built our
+        tools before releasing them, so go ahead and give them a try!</p>
+
+    <section>
+        <h5>Try our dns node!</h5>
+        <p>
+            We have a supervised public DNS node available for hosting. Feel free to utilize it or host your own!
+            Try it at: <span>127.0.0.1</span>
+        </p>
+    </section>
+
+    <!-- This section should be hidden in the release -->
+    <section>
+        <h5>Try our reverse proxy!</h5>
+        <p>To enable universal access without requiring DNS settings changes, we've developed a proxy that acts as the
+            website. Please note that we assume no responsibility for the displayed content.</p>
+        <button>Check it out now!</button>
+    </section>
+</article>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,10 +2,12 @@
     import api from "../utils/api";
     import Intro from "../lib/components/home/Intro.svelte";
     import Empower from "../lib/components/home/Empower.svelte";
+    import TryItOut from "../lib/components/home/TryItOut.svelte";
 </script>
 
 <Intro />
 <Empower />
+<TryItOut />
 
 <pre style="text-align: left">
 {#await api.me()}Loading...
